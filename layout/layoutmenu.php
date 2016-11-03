7cd9aeea29381e98251a4bbe94f379c3f5489e6d
jaczkowska <kjaczkowska@gmail.com>
2016-11-03 17:59:12 +0100

    update layoutmenu.php

--- layout/layoutmenu.php
+++ layout/layoutmenu.php
@@ -30,7 +30,7 @@
 				<span class="icon-bar"></span>
 				<span class="icon-bar"></span>
 			</button>
-			<a class="navbar-brand" href="#">CRMdev</a>
+			<a class="navbar-brand" href="index.php">CRMdev</a>
 		</div>
 		<!-- Collect the nav links, forms, and other content for toggling -->
 		<div class="collapse navbar-collapse" id="bs-sidebar-navbar-collapse-1">
